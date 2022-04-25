17159d0cafcd9d70d47f8ec840f86e7a01d63726
Leandro <102531267+y-leandro@users.noreply.github.com>
2022-04-25 11:48:12 -0300

    Calculando P.A

---
+++ Lógica Computacional/Main (1).java
@@ -0,0 +1,29 @@
+public class Main
+{
+  public static void main (String[]args)
+  {
+    int a1 = 15;
+    int n = 19;
+    int r = 8;
+    int an = a1 + (n - 1) * r;
+
+      System.out.println ("O valor de An é "+an+";");
+
+      a1 = an - (n - 1) * r;
+
+      System.out.println ("O valor de A1 é "+a1+";");
+
+      r = (an - a1) / (n - 1);
+
+      System.out.println ("O valor de r é "+r+";");
+
+      n = (an - a1) / r + 1;
+
+      System.out.println ("O valor de n é "+n+".");
+  }
+}
+
+
+
+
+
